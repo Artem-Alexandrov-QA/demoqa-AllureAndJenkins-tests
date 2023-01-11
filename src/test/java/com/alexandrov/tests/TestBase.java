718e151ef66b4319f2e12d72e512310f44a55eb7
Artem-Alexandrov-QA <artemaleksandrov2676@gmail.com>
2023-01-11 19:22:01 +0400

    added Helpers

--- src/test/java/com/alexandrov/tests/TestBase.java
+++ src/test/java/com/alexandrov/tests/TestBase.java
@@ -1,8 +1,12 @@
 package com.alexandrov.tests;
 
+import com.alexandrov.helpers.Attach;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
 
