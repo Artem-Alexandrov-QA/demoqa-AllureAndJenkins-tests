bcd70a3a933ff58b47ffa82022be0b9d7cab1bfe
Artem-Alexandrov-QA <artemaleksandrov2676@gmail.com>
2023-01-11 19:17:16 +0400

    update gradle

--- src/test/java/com/alexandrov/tests/TestBase.java
+++ src/test/java/com/alexandrov/tests/TestBase.java
@@ -1,6 +1,7 @@
 package com.alexandrov.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
@@ -9,6 +10,26 @@ public class TestBase {
     static void beforeAll() {
         Configuration.startMaximized = true;
         Configuration.baseUrl = "https://demoqa.com";
+    }
+    static void setup() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
+        Configuration.startMaximized = true;
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+    }
+
+    @AfterEach
+    public void tearDown() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
 
