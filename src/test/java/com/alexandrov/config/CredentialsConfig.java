0d90cedcce01bc1a0153635881a168439c6d6e84
Artem-Alexandrov-QA <artemaleksandrov2676@gmail.com>
2023-01-20 12:50:08 +0400

    update TestBase

--- src/test/java/com/alexandrov/config/CredentialsConfig.java
+++ src/test/java/com/alexandrov/config/CredentialsConfig.java
@@ -2,7 +2,9 @@ package com.alexandrov.config;
 
 import org.aeonbits.owner.Config;
 
-@Config.Sources({"classpath:config/credentials.properties"})
+import static org.aeonbits.owner.Config.*;
+
+@Config.Sources("classpath:config/credentials.properties")
 public interface CredentialsConfig extends Config {
 
     String login();
